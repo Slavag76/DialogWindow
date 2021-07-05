7a105d62f859b584138a2582d24e6ec524eeddcf
SlavaGr <Slavag76@yandex.ru>
2021-07-05 16:14:19 +0300

    Initial commit

---
+++ app/src/main/java/com/testlslavag76/dialogwindow/MainActivity.java
@@ -0,0 +1,14 @@
+package com.testlslavag76.dialogwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
